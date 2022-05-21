8a19fef9d2deab2137b2dc93b02882a344bdf64e
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-22 01:28:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ctdj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("carparts").collection("products");
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Car Parts House!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

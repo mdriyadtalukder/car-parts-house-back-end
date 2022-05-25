3696edc91f179dd7dfba02ce5d6e223457a43553
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-26 01:16:40 +0600

    edit

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ async function run() {
 
         });
 
+        
         app.delete('/allproducts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };

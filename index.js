0551ec497ae0847a9cb0a178869f25253fc351eb
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-25 13:44:22 +0600

    add admin

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
             res.send(users);
 
         });
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+
+
+        });
 
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;

8699c9d8b78b03819e6cd4ac9f116f63c7495afa
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-25 19:49:37 +0600

    add pay

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
 
         });
 
-        app.post('/products',  async (req, res) => {
+        app.post('/products', async (req, res) => {
             const newUser = req.body;
             console.log(newUser);
             const result = await userCollection.insertOne(newUser);
@@ -164,6 +164,13 @@ async function run() {
                 res.status(403).send({ message: 'forbidden access' });
             }
         });
+
+        app.get('/myorder/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await orderCollection.findOne(query);
+            res.send(order);
+        })
 
         // Add Orders
 

a9e818be74a7570cf2eca0f506c08fc516da0b0c
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-26 16:50:51 +0600

    edit

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000
 
 const corsConfig = {
-    origin: true,
+    origin: '*',
     credentials: true,
-};
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+})
 app.use(express.json());
 
 function verifyJWT(req, res, next) {
